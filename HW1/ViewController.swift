7c2dd4b25638f466aed50ce7780023a036ed64ac
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-03-29 18:44:27 +0700

    v 1.0.1

--- HW1/ViewController.swift
+++ HW1/ViewController.swift
@@ -6,42 +6,43 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-        let text = UILabel()
-        let nameData = UILabel(); let surnameData = UILabel(); let flowData = UILabel()
-        let defaultData = ("Name", "Surname", "iOS 6");
-        let myData = ("Павел", "Градов", "iOS 7")
+        let name = makeLabel(withText: "Имя:", textColor: .black)
+        name.frame = CGRect(x: 40, y: 80, width: 60, height: 60)
+        view.addSubview(name)
+        
+        let surname = makeLabel(withText: "Фамилия:", textColor: .black)
+        surname.frame = CGRect(x: 40, y: 120, width: 100, height: 60)
+        view.addSubview(surname)
+        
+        let flow = makeLabel(withText: "Номер потока:", textColor: .black)
+        flow.frame = CGRect(x: 40, y: 160, width: 200, height: 60)
+        view.addSubview(flow)
+        
+        let nameData = makeLabel(withText: "Name", textColor: .red)
+        nameData.frame = CGRect(x: 94, y: 80, width: 80, height: 60)
+        view.addSubview(nameData)
+        
+        let surnameData = makeLabel(withText: "Surname", textColor: .red)
+        surnameData.frame = CGRect(x: 142, y: 130, width: 120, height: 40)
+        view.addSubview(surnameData)
+        
+        let flowData = makeLabel(withText: "iOS 6", textColor: .red)
+        flowData.frame = CGRect(x: 189, y: 170, width: 100, height: 40)
+        view.addSubview(flowData)
         
         let addButton = UIButton()
-        lazy var addBtnAction = UIAction { _ in (nameData.text, surnameData.text, flowData.text) = myData }
+        lazy var addBtnAction = UIAction { _ in
+            nameData.text = "Павел"
+            surnameData.text = "Градов"
+            flowData.text = "iOS 7"
+        }
         
         let resetButton = UIButton()
-        lazy var resetBtnAction = UIAction { _ in (nameData.text, surnameData.text, flowData.text) = defaultData }
-        
-        text.frame = CGRect(x: 40, y: 80, width: 200, height: 100)
-        text.numberOfLines = 3
-        text.text = "Имя: \nФамилия: \nНомер потока: "
-        text.font = UIFont.boldSystemFont(ofSize: 20.0)
-        
-        // как внутри одного текста настроить пространство между строками?
-        view.addSubview(text)
-        
-        nameData.frame = CGRect(x: 94, y: 77, width: 80, height: 60)
-        nameData.textColor = .red
-        nameData.font = UIFont.boldSystemFont(ofSize: 20.0)
-        
-        surnameData.frame = CGRect(x: 142, y: 111, width: 120, height: 40)
-        surnameData.textColor = .red
-        surnameData.font = UIFont.boldSystemFont(ofSize: 20.0)
-        
-        flowData.frame = CGRect(x: 189, y: 134, width: 120, height: 40)
-        flowData.textColor = .red
-        flowData.font = UIFont.boldSystemFont(ofSize: 20.0)
-        
-        (nameData.text, surnameData.text, flowData.text) = defaultData
-        
-        view.addSubview(nameData)
-        view.addSubview(surnameData)
-        view.addSubview(flowData)
+        lazy var resetBtnAction = UIAction { _ in
+            nameData.text = "Name"
+            surnameData.text = "Surname"
+            flowData.text = "iOS 6"
+        }
         
         addButton.frame = CGRect(x: 20, y: 650, width: 350, height: 60)
         addButton.setTitle("Добавить данные", for: .normal)
@@ -50,8 +51,6 @@ class ViewController: UIViewController {
         
         addButton.addAction(addBtnAction, for: .touchUpInside)
         
-        // ошибка: Argument of '#selector' does not refer to an '@objc' method, property, or initializer
-        //addButton.addTarget(self, action: #selector(setMyData(nameData, surnameData, flowData)), for: .touchUpInside)
         view.addSubview(addButton)
         
         resetButton.frame = CGRect(x: 20, y: 720, width: 350, height: 60)
@@ -61,6 +60,14 @@ class ViewController: UIViewController {
         resetButton.addAction(resetBtnAction, for: .touchUpInside)
         
         view.addSubview(resetButton)
+    }
+    
+    func makeLabel(withText: String?, textColor: UIColor) -> UILabel {
+        let label = UILabel()
+        label.text = withText ?? ""
+        label.textColor = textColor
+        label.font = UIFont.boldSystemFont(ofSize: 20.0)
+        return label
     }
 }
 
